8eb02f81944aae6bbd42e4bbff41c3a875bcdace
chenzeshu <1193297950@qq.com>
2018-01-08 09:00:30 +0800

    rm laravoole

--- config/app.php
+++ config/app.php
@@ -61,7 +61,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Laravoole\LaravooleServiceProvider::class,
+//        Laravoole\LaravooleServiceProvider::class,
         /*
          * Package Service Providers...
          */
